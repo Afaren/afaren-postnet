16dd63494a47de4c35bf41c55c244b7186b04e1e
Afaren <550047450@qq.com>
2016-07-30 06:45:51 +0800

    modified to OO style

--- src/shell/app.js
+++ src/shell/app.js
@@ -1,67 +1,17 @@
 'use strict';
 
-
+const InitAction = require('./actions/InitAction');
+const BarcodeAction = require('./actions/BarcodeAction');
+const ZipcodeAction = require('./actions/ZipcodeAction');
 const repl = require('repl');
-const core = require('../core/postnet');
+// const core = require('../core/postnet');
 
 const actions = [
-  {
-    name: 'init',
-    help: 'initial: 1. barcode,  2. zipcode',
-    doAction: function (cmd) {
-      switch (cmd) {
-        case '1':
-          return 'barcode';
-        case '2':
-          return 'zipcode';
-        case 'q':
-          console.log('---exit---');
-          replServer.close();
-          process.exit(0);
-          return;
-        default:
-          console.log('无效输入');
-          return 'init';
-      }
-    }
-  },
-  {
-    name: 'barcode',
-    help: 'input a barcode to convert, entry \'q\' to return previous',
-    doAction: function (cmd) {
-      switch (cmd) {
-        case 'q':
-          return 'init';
-        default:
-          console.log('result: ');
-          printConvertResult(core.barcodeToZipcode(cmd));
-          return 'init';
-      }
-    }
-  },
-  {
-    name: 'zipcode',
-    help: 'input a zipcode to convert, entry \'q\' to return previous',
-    doAction: function (cmd) {
-      switch (cmd) {
-        case 'q':
-          return 'init';
-        default:
-          console.log('result: ');
-          printConvertResult(core.zipcodeToBarcode(cmd));
-          return 'init';
-      }
-    }
-  }
+  new InitAction(),
+  new BarcodeAction(),
+  new ZipcodeAction()
 ];
 
-function printConvertResult(result) {
-  if (result.errMsg !== null)
-    console.log('error message: ' + result.errMsg);
-  else
-    console.log('<--  ' + result.value);
-  return;
-}
 
 function switchRouter(context, done) {
   let router = actions.find(i => i.name === currentState);
@@ -75,7 +25,6 @@ function switchRouter(context, done) {
 
 function handleCmd(cmd, context, filename, done) {
   switchRouter({cmd: cmd.trim()}, done);
-
   done(null);
 }
 
