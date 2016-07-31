2057795bfdc5cd752d256094c7baf7d2f6f5e0d5
Afaren <550047450@qq.com>
2016-07-31 23:24:21 +0800

    complete style change to OO

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 /**
  * Created by afaren on 7/31/16.
  */
-const Router = require('./Router');
-const repl = require('repl');
-const Shell = require('./shell/Shell');
 
-new Shell(new Router(), repl).run();
+const repl = require('repl');
+const Router = require('./Router');
+const Shell = require('./shell/Shell');
+const InitAction = require('./actions/InitAction');
+const BarcodeAction = require('./actions/BarcodeAction');
+const ZipcodeAction = require('./actions/ZipcodeAction');
+
+
+let actions = [];
+actions.push(new InitAction());
+actions.push(new BarcodeAction());
+actions.push(new ZipcodeAction());
+
+
+const router = new Router(actions);
+const shell = new Shell(router, repl);
+shell.run();
